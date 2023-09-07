f0a5e6f051a88f9ac1ea0cf6c71e663f8ace29d7
VictorAmpir <140673272+VictorAmpir@users.noreply.github.com>
2023-09-07 20:03:27 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.WriteLine("№47. Задайте двумерный массив размером m*n, заполненный случайными вещественными числами");
+Console.WriteLine(" ");
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers47 = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers47);
+PrintArray(numbers47);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine(" ");
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Задача 50. Программа, которая на вход принимает позиции элемента в двумерном массиве,");
+Console.WriteLine(" и возвращает значение этого элемента или же указание, что такого элемента нет.");
+Console.WriteLine(" ");
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers50 = new int [10,10];
+FillArrayRandomNumbers50(numbers50);
+
+if (n > numbers50.GetLength(0) || m > numbers50.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers50[n-1,m-1]}");
+}
+
+PrintArray50(numbers50);
+
+void FillArrayRandomNumbers50(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray50(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+
+Console.WriteLine(" ");
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
+Console.WriteLine(" ");
+Console.WriteLine("введите количество строк");
+int h = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers52 = new int[h, k];
+FillArrayRandomNumbers52(numbers52);
+
+
+for (int j = 0; j < numbers52.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers52.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers52[i, j]);
+    }
+    avarage = avarage / h;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray52(numbers52);
+
+
+
+void FillArrayRandomNumbers52(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray52(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
